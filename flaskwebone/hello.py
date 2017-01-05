85b919b5f312bc040dc82bc6bb764c7f2cb5a249
GuoMaoFei <1025006407@qq.com>
2017-01-05 11:17:56 +0800

    基本
    还没有表单

--- flaskwebone/hello.py
+++ flaskwebone/hello.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask,render_template
+from  flask.ext.bootstrap import Bootstrap
+from  flask.ext.wtf import  Form
+from  wtforms import StringField,SubmitField
+from  wtforms.validators import Required
+
 app=Flask(__name__)
+bootstrap=Bootstrap(app)
+app.config['SECRET_KEY']='hard to guess string'
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return  render_template('user.html',name=name)
+
+class NameForm(Form):
+    name=StringField('What is your name?',validators=[Required()])
+    submit=SubmitField('Submit')
 
 if __name__=='__main__':
     app.run(debug=True)
