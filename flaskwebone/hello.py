b4bb69f4fffb7490715d823cf2025ddc8ed2eea7
GuoMaoFei <1025006407@qq.com>
2017-01-05 14:24:03 +0800

    新的
    POST重新定向，flash消息和部分数据库

--- flaskwebone/hello.py
+++ flaskwebone/hello.py
@@ -1,21 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from  flask.ext.bootstrap import Bootstrap
 from  flask.ext.wtf import  Form
 from  wtforms import StringField,SubmitField
 from  wtforms.validators import Required
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
 
+basedir=os.path.abspath(os.path.dirname(__file__))
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
 app.config['SECRET_KEY']='hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+db=SQLAlchemy(app)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    name=None
     form=NameForm()
     if form.validate_on_submit():
-        name=form.name.data
-        form.name.data=''
-    return render_template('index.html',form=form,name=name)
+        old_name=session.get('name')
+        if old_name is not None and old_name !=form.name.data:
+            flash('Looks like you have changed your name')
+        session['name']=form.name.data
+        return  redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
